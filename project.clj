b13b913a2b44201bb53b9a35ab0651d0df5fceae
Tsutomu YANO <tsutomuyano@shelfinc.com>
2022-05-21 17:23:44 +0900

    Changed the version and lib name to tyano/struct "1.5.0"
    Bump-up library versions.

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
-(defproject funcool/struct "1.3.0"
+(defproject tyano/struct "1.5.0"
   :description "A structural validation library for Clojure(Script)"
-  :url "https://github.com/funcool/struct"
-  :license {:name "Public Domain" :url "http://unlicense.org/"}
-  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
-                 [funcool/cuerdas "2.0.5"]]
+  :url "https://github.com/tyano/struct"
+  :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.11.51" :scope "provided"]
+                 [funcool/cuerdas "2.2.1"]]
   :source-paths ["src"]
   :test-paths ["test"]
   :codeina {:sources ["src"]
             :reader :clojure
             :target "doc/dist/latest/api"}
   :plugins [[funcool/codeina "0.5.0"]
-            [lein-ancient "0.6.15" :exclusions [org.clojure/tools.reader]]])
+            [lein-ancient "0.7.0" :exclusions [org.clojure/tools.reader]]]
+  
+  :profiles {:dev {:source-paths ["dev/src"]}})
